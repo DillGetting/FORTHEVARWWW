d1f58775171b6754b2640042eadd9e854470cafc
Dillon Wilcox <83142241+DillGetting@users.noreply.github.com>
2021-12-10 07:54:14 -0700

    Create insertcoininfo.php
    form for student info

---
+++ insertcoininfo.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<h3>Insert a student</h3>
+<form action="/insertstudent.php" method="get">
+  <label for="id">Student ID:</label><br>
+  <input type="text" id="id" name="id"><br>
+  <label for="first_name">First Name: </label><br>
+  <input type="text" id="first_name" name="first_name"><br>
+  <label for="last_name">Last Name: </label><br>
+  <input type="text" id="last_name" name="last_name">
+  <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
