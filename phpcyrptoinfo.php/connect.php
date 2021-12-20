934fac4e122cebd57a16b3df7b0912ecbf98300d
Dillon Wilcox <83142241+DillGetting@users.noreply.github.com>
2021-12-20 02:21:03 -0700

    mysqli-connect.php

--- phpcyrptoinfo.php/connect.php
+++ phpcyrptoinfo.php/connect.php
@@ -1,9 +1,9 @@
 <?php
 // Login constants
-DEFINE ('DB_USER', 'dillon');
+DEFINE('DB_USER', 'dildev');
 DEFINE('DB_PASSWORD', 'b');
 DEFINE('DB_HOST', 'localhost');
-DEFINE('DB_NAME', 'studentdatabase');
+DEFINE('DB_NAME', 'wordbase1');
 
 // create the database connection, the @ symbol prevents errors from being output, instead printing the die sta>
 // This can be used to prevent connection information from leaking to the user
